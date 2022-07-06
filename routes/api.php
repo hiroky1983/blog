5e270ea05eefe89c3de81d0fb4dc98acb56cd82c
yamadahiroki <hirockysan1983@gmail.com>
2022-07-07 00:01:03 +0900

    API更新

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'BlogController@index')->name('blogs');
+Route::get('/',[BlogController::class , 'index']);
